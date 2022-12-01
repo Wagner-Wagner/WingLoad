d04c9b3d3ea045341b68dd0242f37e648ab57bf1
Wagner-Wagner <dewilde.olivier@outlook.com>
2022-12-01 09:59:07 +0100

    Fin projet wingload calculator

--- app-anim.js
+++ app-anim.js
@@ -10,11 +10,7 @@ let categorie5 = document.querySelector(".categorie5");
 let categorie6 = document.querySelector(".categorie6");
 let categorie7 = document.querySelector(".categorie7");
 
-
-
-
-
-let x = cat.forEach(function (element, index) {
+cat.forEach(function (element, index) {
     element.addEventListener("click", function(){
         switch (index) {
             case 0:
@@ -28,22 +24,27 @@ let x = cat.forEach(function (element, index) {
             case 2:
                 addClass()
                 categorie3.classList.remove("remove");
+                
                 break;
             case 3:
                 addClass()
                 categorie4.classList.remove("remove");
+                
                 break;
             case 4:
                 addClass()
                 categorie5.classList.remove("remove");
+                
                 break;
             case 5:
                 addClass()
                 categorie6.classList.remove("remove");
+                
                 break;  
             case 6:
                 addClass()
                 categorie7.classList.remove("remove");
+                
             default:
                 break;
         }
@@ -51,8 +52,39 @@ let x = cat.forEach(function (element, index) {
 })
 
 
-
 function addClass() {
     categories.classList.add("active")
+    crossDiv.classList.remove("none")
 }
 
+
+//cross animation close
+
+const crossDiv = document.querySelector(".cross");
+const allCategorie =document.querySelectorAll("#nbrCategorie");
+
+
+crossDiv.addEventListener("click", () =>{
+    crossDiv.classList.add("active-span")
+    categories.classList.remove("active")
+    setTimeout(() => {
+        crossDiv.classList.add("none")  
+        crossDiv.classList.remove("active-span")  
+    }, 500);
+    for (let i = 0; i < allCategorie.length; i++) {
+        const element = allCategorie[i].classList.contains("remove");
+        if (element == false) {
+            allCategorie[i].classList.add("remove")
+        }
+    }
+})
+
+
+
+// Retour vers le wingload calculator
+
+const wingload = document.querySelector(".wingload");
+
+wingload.addEventListener("click", () => {
+    document.location.href = "index.html"
+})
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector("#btn");
 const btn2 = document.querySelector("#btn2");
 const resultat = document.querySelector(".resultat");
+const resultatCategorie = document.querySelector(".resultatCategorie");
 const coursSousVoile = document.querySelector("#cours-sous-voile");
 
 let nbrJumps = document.querySelector("#nbrJumps");
@@ -17,7 +18,8 @@ btn.addEventListener("click", submit);
 
 function submit() {
 
-        resultat.textContent = " "   
+        resultat.textContent = " " 
+        resultatCategorie.textContent = " "  
         checked()
         weight.value = " "
         size.value = " "
@@ -49,28 +51,36 @@ function isChecked() {
         alert("Tu dois avoir plus de 0 saut")
     }else if (valueNbrJumps >= 1 && valueNbrJumps <= 25) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.1`
+        resultatCategorie.textContent = "Catégoe de voile MAX 2"
         console.log("+= 1 et -= 25");
     }else if (valueNbrJumps >= 26 && valueNbrJumps <= 50) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.2`
+        resultatCategorie.textContent = "Catégoe de voile MAX 2"
         console.log("+=26 et -= 50");
     }else if (valueNbrJumps >= 51 && valueNbrJumps <= 100) {
         console.log("+= 51 et -=100");
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.3`
+        resultatCategorie.textContent = "Catégoe de voile MAX 3"
     }else if (valueNbrJumps >= 101 && valueNbrJumps <= 200) {
         console.log("+= 101 et -= 200");
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.4`
+        resultatCategorie.textContent = "Catégoe de voile MAX 3"
     }else if (valueNbrJumps >= 201 && valueNbrJumps <= 500) {
         console.log("+= 201 et -= 500");
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.6`
+        resultatCategorie.textContent = "Catégoe de voile MAX 4"
     }else if (valueNbrJumps >= 501 && valueNbrJumps <= 1000) {
         console.log("+= 501 et -=1000");
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.9`
+        resultatCategorie.textContent = "Catégoe de voile MAX 5"
     }else if (valueNbrJumps >= 1001 && valueNbrJumps <= 2000) {
         console.log("+= 1001 et -= 2000");
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 2.5`
+        resultatCategorie.textContent = "Catégoe de voile MAX 6"
     }else if (valueNbrJumps >= 2000) {
-        console.log("+++2000");
         resultat.textContent = `Ton wingload est de ${r} `
+        resultatCategorie.textContent = "Catégoe de voile MAX 7"
+        console.log("+++2000");
     }else{
         console.log("erreur");
     }
@@ -89,27 +99,35 @@ function isNotChecked() {
         alert("Tu dois avoir plus de 0 saut")
     }else if (valueNbrJumps >= 1 && valueNbrJumps <= 25) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1`
+        resultatCategorie.textContent = "Catégoe de voile MAX 1"
         console.log("NNN += 1 et -= 25");
     }else if (valueNbrJumps >= 26 && valueNbrJumps <= 50) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.1`
+        resultatCategorie.textContent = "Catégoe de voile MAX 2"
         console.log("NNN +=26 et -= 50");
     }else if (valueNbrJumps >= 51 && valueNbrJumps <= 100) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.2`
+        resultatCategorie.textContent = "Catégoe de voile MAX 3"
         console.log("NNN += 51 et -=100");
     }else if (valueNbrJumps >= 101 && valueNbrJumps <= 200) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.3`
+        resultatCategorie.textContent = "Catégoe de voile MAX 3"
         console.log("NNN += 101 et -= 200");
     }else if (valueNbrJumps >= 201 && valueNbrJumps <= 500) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.5`
+        resultatCategorie.textContent = "Catégoe de voile MAX 3"
         console.log("NNN += 201 et -= 500");
     }else if (valueNbrJumps >= 501 && valueNbrJumps <= 1000) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.7`
+        resultatCategorie.textContent = "Catégoe de voile MAX 4"
         console.log("NNN += 501 et -=1000");
     }else if (valueNbrJumps >= 1001 && valueNbrJumps <= 2000) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 1.9`
+        resultatCategorie.textContent = "Catégoe de voile MAX 5"
         console.log("NNN += 1001 et -= 2000");
     }else if (valueNbrJumps >= 2000) {
         resultat.textContent = `Ton wingload est de ${r} il devrait etre de MAX 2.5`
+        resultatCategorie.textContent = "Catégoe de voile MAX 6"
         console.log("NNN +++2000");
     }else{
         console.log("erreur");
